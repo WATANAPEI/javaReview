a4da1aa7e65501a34f3ac77c0b3958b960ca5f48
WATANAPEI <info@wpei.dev>
2020-05-09 17:30:13 +0900

    finished Flashcard #4

--- Flashcards/Flashcards/task/src/flashcards/Main.java
+++ Flashcards/Flashcards/task/src/flashcards/Main.java
@@ -1,31 +1,69 @@
 package flashcards;
-
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input the number of cards:");
-        String cards = scanner.nextLine();
-        int cardNum = Integer.parseInt(cards);
-        String[] terms = new String[cardNum];
-        String[] definitions = new String[cardNum];
-        for(int i = 0; i < cardNum; i++) {
-            System.out.println("The card #" + (i+1) + ":");
-            terms[i] = scanner.nextLine();
-            System.out.println("The definition of the card #" + (i+1) + ":");
-            definitions[i] = scanner.nextLine();
-        }
-        for(int i = 0; i < cardNum; i++) {
-            System.out.println("Print the definition of \"" + terms[i] + "\":");
-            String ans = scanner.nextLine();
-            if(ans.equals(definitions[i])) {
-                System.out.println("Correct answer.");
+
+    public static String getCardString(Map<String, String> card) {
+           Scanner sc = new Scanner(System.in);
+           System.out.println("The card #" + (card.size() +1) + ":");
+           while(true)
+           {
+               String input = sc.nextLine();
+               if(card.containsKey(input)) {
+                   System.out.println("The card \"" + input + "\" already exists. Try again:");
+               } else {
+                   return input;
+               }
+           }
+    }
+
+    public static String getDefString(Map<String, String> card) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("The definition of the card #" + (card.size()+1) + ":");
+        while(true)
+        {
+            String input = sc.nextLine();
+            if(card.containsValue(input)) {
+                System.out.println("The definition \"" + input + "\" already exists. Try again:");
             } else {
-                System.out.println("Wrong answer. The correct one is \"" + definitions[i] + "\".");
-
+                return input;
             }
+        }
+    }
 
+    public static void main(String[] args) {
+        System.out.println("Input the number of cards:");
+        Scanner sc = new Scanner(System.in);
+        String cards = sc.nextLine();
+        int cardNum = Integer.parseInt(cards);
+        LinkedHashMap<String, String> card = new LinkedHashMap<>();
+        for(int i = 0; i < cardNum; i++) {
+            String key = getCardString(card);
+            String val = getDefString(card);
+            card.put(key, val);
+        }
+        int i = 0;
+        for(Map.Entry<String, String> e : card.entrySet()) {
+            System.out.println("Print the definition of \"" + e.getKey() + "\":");
+            String ans = sc.nextLine();
+            if(ans.equals(e.getValue())) {
+                System.out.println("Correct answer.");
+            } else if (card.containsValue(ans)) {
+                StringBuilder def = new StringBuilder();
+                for(String k: card.keySet()) {
+                    if (card.get(k).equals(ans)) {
+                        def.append(k);
+                    }
+                }
+                System.out.println("Wrong answer. The correct one is \"" + e.getValue()
+                        + "\", you've just written the definition of \"" + def + "\".");
+
+            } else {
+                System.out.println("Wrong answer. The correct one is \"" + e.getValue() + "\".");
+            }
         }
     }
 }
