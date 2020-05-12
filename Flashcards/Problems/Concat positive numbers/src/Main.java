28f867fed837adb158c0f5e1c66d06b371467077
WATANAPEI <info@wpei.dev>
2020-05-12 23:49:05 +0900

    add some solutions

---
+++ Flashcards/Problems/Concat positive numbers/src/Main.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collectors;
+
+class ConcatPositiveNumbersProblem {
+
+    public static ArrayList<Integer> concatPositiveNumbers(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+        l1.addAll(l2);
+        List<Integer> l = l1.stream()
+                .filter(e -> e > 0)
+                .collect(Collectors.toList());
+        return (ArrayList<Integer>) l; // write your code here
+    }
+
+    /* Do not modify this method */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list1 = readArrayList(scanner);
+        ArrayList<Integer> list2 = readArrayList(scanner);
+
+        ArrayList<Integer> result = concatPositiveNumbers(list1, list2);
+
+        result.forEach((n) -> System.out.print(n + " "));
+    }
+
+    /* Do not modify this method */
+    private static ArrayList<Integer> readArrayList(Scanner scanner) {
+        return Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+}
