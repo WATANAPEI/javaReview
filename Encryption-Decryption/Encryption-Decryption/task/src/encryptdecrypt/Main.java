42caa5e7bc5874bd347cff45f724a2e42bc7ecc3
WATANAPEI <info@wpei.dev>
2020-05-20 22:05:46 +0900

    solved task #2

--- Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,14 +1,28 @@
 package encryptdecrypt;
 
+import java.io.*;
+import java.nio.Buffer;
+
 public class Main {
+
     public static void main(String[] args) {
-        String msg = "we found a treasure!";
+        StringBuilder inputStr = new StringBuilder();
+        int shiftNumber = 0;
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+                String line = br.readLine();
+                inputStr.append(line);
+                shiftNumber = Integer.parseInt(br.readLine());
+            } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        String msg = inputStr.toString();
         StringBuilder sb = new StringBuilder();
+
         for(char c: msg.toCharArray()) {
             if(Character.isAlphabetic(c)) {
-                int distanceFromA = c - 'a';
-                //System.out.println(distanceFromA);
-                char encryptedChar = (char)('z' - distanceFromA);
+                int shiftedCharNum = ( c - 'a' + shiftNumber ) % ('z' - 'a' + 1) + 'a' ;
+                //System.out.println(shiftedCharNum);
+                char encryptedChar = (char)(shiftedCharNum);
                 //System.out.println(encryptedChar);
                 sb.append(encryptedChar);
             } else {
@@ -16,5 +30,6 @@ public class Main {
             }
         }
         System.out.println(sb.toString());
+
     }
 }
