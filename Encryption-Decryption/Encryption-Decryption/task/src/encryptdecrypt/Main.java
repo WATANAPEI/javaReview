dd68d4460f681bccdd55b181b17771f83dffb593
WATANAPEI <info@wpei.dev>
2020-05-20 23:53:55 +0900

    refactored (changed to use command-map to get rid of if statement)

--- Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -4,7 +4,9 @@ import java.io.*;
 import java.lang.reflect.Array;
 import java.nio.Buffer;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -52,16 +54,54 @@ public class Main {
 
     public static void main(String[] args) {
         List<String> list = getInputLines();
-        int shiftNumber = Integer.parseInt(list.get(2));
         String mode = list.get(0);
-        if(mode.contentEquals("enc")) {
-            System.out.println(encryptCaesar(list.get(1), shiftNumber));
-        } else if (mode.contentEquals("dec")) {
-            System.out.println(decryptCaesar(list.get(1), shiftNumber));
-        } else {
-            System.out.println("Unknown command");
-        }
-
+        String stringToConvert = list.get(1);
+        int shiftNumber = Integer.parseInt(list.get(2));
+        Map<String, Command> commandMap = new HashMap();
+        commandMap.put("enc", Command.Enc);
+        commandMap.put("dec", Command.Dec);
+        String convertedStr = commandMap.get(mode).convertStr(stringToConvert, shiftNumber);
+        System.out.println(convertedStr);
 
     }
 }
+
+enum Command {
+    Enc {
+        @Override
+        public String convertStr(String in, int shiftNumber) {
+            StringBuilder sb = new StringBuilder();
+            for(char c: in.toCharArray()) {
+                int shiftedCharNum = c + shiftNumber;
+                //System.out.println(shiftedCharNum);
+                char encryptedChar = (char)(shiftedCharNum);
+                //System.out.println(encryptedChar);
+                sb.append(encryptedChar);
+            }
+            return sb.toString();
+
+        }
+    },
+    Dec {
+        @Override
+        public String convertStr(String in, int shiftNumber) {
+            StringBuilder sb = new StringBuilder();
+            for(char c: in.toCharArray()) {
+                int shiftedCharNum = c - shiftNumber;
+                //System.out.println(shiftedCharNum);
+                char encryptedChar = (char)(shiftedCharNum);
+                //System.out.println(encryptedChar);
+                sb.append(encryptedChar);
+            }
+            return sb.toString();
+
+        }
+
+    };
+
+    public abstract String convertStr(String in, int shiftNumber);
+}
+
+
+
+
