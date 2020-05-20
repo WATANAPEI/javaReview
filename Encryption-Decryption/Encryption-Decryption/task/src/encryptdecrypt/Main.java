1d9247db9fd24b9f21b2b8f83a4982ea6f7b7a3c
WATANAPEI <info@wpei.dev>
2020-05-20 22:30:37 +0900

    solved task #3

--- Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,35 +1,67 @@
 package encryptdecrypt;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
-        StringBuilder inputStr = new StringBuilder();
-        int shiftNumber = 0;
+    public static List<String> getInputLines() {
+        String mode = null;
+        String targetStrings = null;
+        String shiftNumber = null;
+        List<String> list = new ArrayList<>();
         try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-                String line = br.readLine();
-                inputStr.append(line);
-                shiftNumber = Integer.parseInt(br.readLine());
-            } catch (IOException ioException) {
+            mode = br.readLine();
+            list.add(mode);
+            targetStrings = br.readLine();
+            list.add(targetStrings);
+            shiftNumber = br.readLine();
+            list.add(shiftNumber);
+        } catch (IOException ioException) {
             ioException.printStackTrace();
         }
-        String msg = inputStr.toString();
-        StringBuilder sb = new StringBuilder();
+        return list;
+    }
 
-        for(char c: msg.toCharArray()) {
-            if(Character.isAlphabetic(c)) {
-                int shiftedCharNum = ( c - 'a' + shiftNumber ) % ('z' - 'a' + 1) + 'a' ;
-                //System.out.println(shiftedCharNum);
-                char encryptedChar = (char)(shiftedCharNum);
-                //System.out.println(encryptedChar);
-                sb.append(encryptedChar);
-            } else {
-                sb.append(c);
-            }
+    public static String encryptCaesar(String in, int shiftNumber) {
+        StringBuilder sb = new StringBuilder();
+        for(char c: in.toCharArray()) {
+            int shiftedCharNum = c + shiftNumber;
+            //System.out.println(shiftedCharNum);
+            char encryptedChar = (char)(shiftedCharNum);
+            //System.out.println(encryptedChar);
+            sb.append(encryptedChar);
         }
-        System.out.println(sb.toString());
+        return sb.toString();
+    }
+
+    public static String decryptCaesar(String in, int shiftNumber) {
+        StringBuilder sb = new StringBuilder();
+        for(char c: in.toCharArray()) {
+            int shiftedCharNum = c - shiftNumber;
+            //System.out.println(shiftedCharNum);
+            char encryptedChar = (char)(shiftedCharNum);
+            //System.out.println(encryptedChar);
+            sb.append(encryptedChar);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        List<String> list = getInputLines();
+        int shiftNumber = Integer.parseInt(list.get(2));
+        String mode = list.get(0);
+        if(mode.contentEquals("enc")) {
+            System.out.println(encryptCaesar(list.get(1), shiftNumber));
+        } else if (mode.contentEquals("dec")) {
+            System.out.println(decryptCaesar(list.get(1), shiftNumber));
+        } else {
+            System.out.println("Unknown command");
+        }
+
 
     }
 }
