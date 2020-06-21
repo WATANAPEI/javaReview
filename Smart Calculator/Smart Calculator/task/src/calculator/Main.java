aa3de47fb26078a485f29c0813ee1d8cf9c104eb
WATANAPEI <info@wpei.dev>
2020-06-21 20:48:27 +0900

    changed architecture but need to fix bugs

--- Smart Calculator/Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/Smart Calculator/task/src/calculator/Main.java
@@ -3,7 +3,223 @@ package calculator;
 import java.text.ParseException;
 import java.util.Scanner;
 
+enum TokenType {
+    NUM,
+    OP;
+}
+enum OpType {
+    ADD,
+    SUB;
+}
+/**
+ * Token
+ * Token type should be String(Operator) or Integer(Number)
+ */
+abstract class Token {
+    TokenType type;
+}
 
+class NumToken extends Token {
+    private Integer value;
+    private String image;
+    NumToken(Integer value) {
+        this.value = value;
+        this.type = TokenType.NUM;
+        this.image = value.toString();
+    }
+    public Integer getValue() {
+        return this.value;
+    }
+    public String toString() {
+        return this.image;
+    }
+}
+
+class OpToken extends Token {
+    private OpType translatedOp;
+    private String image;
+
+    OpToken(String value) {
+        this.type = TokenType.OP;
+        this.image = value;
+        this.translatedOp = translate(value);
+    }
+    OpType translate(String value) {
+        OpType operator = OpType.ADD;
+        for(var e: value.split("")) {
+            if(e.contentEquals("+")) {
+                operator = OpType.ADD;
+            } else if (e.contentEquals("-")) {
+                if(operator == OpType.SUB) {
+                    operator = OpType.ADD;
+                }else {
+                    operator = OpType.SUB;
+                }
+            } else {
+                new ParseException("Parse Error at OperatorNode", 0);
+            }
+        }
+        return operator;
+    }
+    OpType getValue() {
+        return this.translatedOp;
+    }
+
+    public String toString() {
+        return this.image;
+    }
+}
+class Lexer {
+    private Scanner sc;
+
+    Lexer(String line) {
+        sc = new Scanner(line);
+    }
+    Token nextToken() {
+        if(sc.hasNext()) {
+            String nextStr = sc.next();
+            if(nextStr.matches("\\d+")) {
+                return new NumToken(Integer.parseInt(nextStr));
+            } else {
+                return new OpToken(nextStr);
+            }
+        } else {
+            return  null;
+        }
+    }
+
+}
+
+abstract class AST {
+    abstract Integer accept(NodeVisitor v);
+}
+
+class BinOpNode extends AST {
+    AST left;
+    OpType op; //this node
+    AST right;
+
+    BinOpNode(AST left, OpType op, AST right) {
+        this.left = left;
+        this.op = op;
+        this.right = right;
+    }
+
+    @Override
+    Integer accept(NodeVisitor v) {
+        return v.visit(this);
+    }
+}
+
+class NumNode extends AST {
+    Integer value;
+    NumNode(Integer value) {
+        this.value = value;
+    }
+
+    @Override
+    Integer accept(NodeVisitor v) {
+        return v.visit(this);
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
+
+// BNF
+// <Expr> := <Number> | (<Op> <Number>)*
+// <Op> := (+ | -)*
+
+class Parser {
+    Lexer lexer;
+    Token currentToken;
+    Parser(Lexer lexer) {
+        this.lexer = lexer;
+        this.currentToken = lexer.nextToken();
+    }
+
+    /**
+     * consume
+     * @param tokenType
+     * @throws ParseException
+     * advance current token point only when token type is right
+     */
+    void consume(TokenType tokenType) throws ParseException{
+        if(currentToken.type == tokenType) {
+            currentToken = lexer.nextToken();
+        } else {
+            throw new ParseException("Invalid expression", 0);
+        }
+    }
+
+    NumNode parseNum() throws ParseException{
+        Token token = currentToken;
+        try {
+            consume(TokenType.NUM);
+        } catch (ParseException e) {
+            throw e;
+        }
+        Integer i = ((NumToken)token).getValue();
+        return new NumNode(i);
+    }
+
+    AST parseExpr() throws ParseException{
+        AST node = parseNum();
+        while(currentToken.type == TokenType.OP) {
+            Token opToken = currentToken;
+            try {
+                consume(TokenType.OP);
+            } catch (ParseException e) {
+                throw e;
+            }
+            node = new BinOpNode(node, ((OpToken)opToken).getValue(), parseNum());
+            if(currentToken == null) {
+                break;
+            }
+        }
+        return node;
+    }
+
+    AST parse() throws ParseException{
+        return parseExpr();
+    }
+}
+
+/**
+ * NodeVisitor
+ *
+ */
+interface NodeVisitor {
+    Integer visit(BinOpNode node);
+    Integer visit(NumNode node);
+
+}
+
+class Interpreter implements NodeVisitor {
+    Parser parser;
+    Interpreter(Parser parser) {
+        this.parser  = parser;
+    }
+
+    public Integer visit(BinOpNode node) {
+        if(node.op == OpType.ADD) {
+            return node.left.accept(this) + node.right.accept(this);
+        } else {
+            return node.left.accept(this) - node.right.accept(this);
+        }
+    }
+
+    public Integer visit(NumNode node) {
+        return node.getValue();
+    }
+
+    Integer interpret() throws ParseException{
+        AST tree = this.parser.parse();
+        return tree.accept(this);
+
+    }
+}
 
 public class Main {
 
@@ -18,16 +234,18 @@ public class Main {
             } else if (line.contentEquals("/exit")) {
                 exit();
                 break; //terminate program here
+            } else if(line.contains("/") && !line.contentEquals("/exit") && !line.contentEquals("/help")) {
+                unknown();
             } else {
-                Node node = new ExprNode();
+                Lexer lexer = new Lexer(line); // generate tokens
+                Parser parser = new Parser(lexer); // generate AST
+                Interpreter interpreter = new Interpreter(parser);
                 try {
-                    node.parse(new Context(line)); //construct BST
-                    CASTVisitor v = new CalcVisitor();
-                    System.out.println(v.visit((ExprNode)node));
-                    //System.out.println(node.toString());
-                } catch (ParseException e) {
-                    e.printStackTrace();
+                    System.out.println(interpreter.interpret());
+                } catch(ParseException e) {
+                    System.out.println(e.getMessage());
                 }
+                //System.out.println(node.toString());
             }
         }
     }
@@ -47,5 +265,8 @@ public class Main {
 
     public static void exit() {
         System.out.println("Bye!");
+    }
+    public static void unknown() {
+        System.out.println("Unknown command");
     }
 }
